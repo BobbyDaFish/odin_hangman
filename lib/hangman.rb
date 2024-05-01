3adfa021f748c527a30b1519eea867a95e17c0a2
BobbyDaFish <130507595+BobbyDaFish@users.noreply.github.com>
2024-05-01 14:34:21 -0600

    Create new answer method and add dictionary file

---
+++ lib/hangman.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+require 'pry-byebug'
+
+def new_answer
+  word_list = File.readlines('word_dictionary.txt')
+  answer = word_list[rand(word_list.length)].chop
+
+  while answer.length > 12 || answer.length < 5
+    answer = word_list[rand(word_list.length)].chop
+  end
+  answer
+end
