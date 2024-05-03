1fe452505e86a1a1eff0ec6125b3830e5a08ccb0
BobbyDaFish <130507595+BobbyDaFish@users.noreply.github.com>
2024-05-03 11:14:22 -0600

    Add gameplay logic, save file and load prompts
    Added methods to process user input to verify if correct, remove health,
    or update the board. Logic for playing the game and ending it. Added
    methods to check for whether a user wants to save.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -38,17 +38,65 @@ class Game
   end
 
   def get_player_choice(state)
-    puts 'Choose a letter'
+    p state[:alphabet]
+    puts 'Choose a letter from the remaining alphabet'
     valid_player_choice(gets.chop.downcase, state)
   end
 
   def valid_player_choice(letter, state)
     l = letter
     until state[:alphabet].any?(l)
-      puts 'Invalid choice, please select one letter from the available letters.'
+      puts "-----\n\nInvalid choice, please select one letter from the available letters."
       l = gets.chop.downcase
     end
     l
+  end
+
+  def check_play(player_choice, state)
+    puts "-----\n\nThat letter has already been used!" unless state[:alphabet].any?(player_choice)
+    if state[:answer].any?(player_choice)
+      puts "-----\n\nYes! Marking that letter."
+      state = process_play(player_choice, state)
+    else
+      puts "-----\n\nSorry, not that one!"
+      state[:health] -= 1
+    end
+    state[:alphabet][state[:alphabet].index(player_choice)] = '_'
+    state
+  end
+
+  def process_play(player_choice, state)
+    state[:answer].each_with_index do |l, i|
+      next unless l == player_choice
+
+      state[:answer][i] = '_'
+      state[:board][i] = l
+    end
+    state
+  end
+
+  def load?(string, state)
+    until [string].include?('y') || [string].include?('n')
+      puts "-----\n\nInvalid selection.\nDo you want to load your game? Input Y for yes, N for no."
+      string = gets.chop.downcase
+    end
+    if string == 'y'
+      load_save_file
+    elsif string == 'n'
+      state
+    end
+  end
+
+  def save?(string, state)
+    until [string].include?('y') || [string].include?('n')
+      puts "-----\n\nInvalid selection.\nDo you want to save your game? Input Y for yes, N for no."
+      string = gets.chop.downcase
+    end
+    if string == 'y'
+      save_game(state[:answer], state[:board], state[:health], state[:alphabet])
+    elsif string == 'n'
+      state
+    end
   end
 end
 
@@ -60,5 +108,19 @@ state = {
   alphabet: %w[a b c d e f g h i j k l m n o p q r s t u v w x y z]
 }
 state[:board] = Array.new(state[:answer].length, '_')
-# hangman.save_game(state[:answer], state[:board], state[:health], state[:alphabet])
-# state = hangman.load_save_file
+if File.exist?('../save_game.json')
+  puts 'Save game file found. Do you want to load your game? Input Y for yes, N for no.'
+  state = hangman.load?(gets.chop.downcase, state)
+  puts "-----\n\nGame loaded!\n#{state[:board]}\nHealth: #{state[:health]}"
+end
+while state[:board].any?('_') && state[:health].positive?
+  state = hangman.check_play(hangman.get_player_choice(state), state)
+  puts "#{state[:board]}\n Health remaining: #{state[:health]}"
+  puts 'Do you want to save your game?'
+  hangman.save?(gets.chop.downcase, state)
+end
+if state[:health].zero?
+  puts 'You\'re out of health, you lose!'
+elsif state[:board].any?('_') == false
+  puts "You got the word! It\'s #{state[:board].join}!"
+end
